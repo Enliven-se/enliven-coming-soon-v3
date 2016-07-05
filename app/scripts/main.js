3bf73599df4b20d8f6001d8c6f5bbb273d1618aa
klokie <github@klokie.com>
2016-07-06 01:02:26 +0200

    deployed

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -456,11 +456,11 @@ var $mcj = {};
   // MailChimp
   window.fnames = new Array();
   window.ftypes = new Array();
-  fnames[0] = "EMAIL";
-  ftypes[0] = "email";
-  fnames[1] = "FNAME";
-  ftypes[1] = "text";
-  fnames[2] = "LNAME";
-  ftypes[2] = "text";
+  fnames[0] = 'EMAIL';
+  ftypes[0] = 'email';
+  fnames[1] = 'FNAME';
+  ftypes[1] = 'text';
+  fnames[2] = 'LNAME';
+  ftypes[2] = 'text';
 
 })(jQuery);
