989900eb266a90d4483bcd326ab50b758af8fcc1
klokie <github@klokie.com>
2016-08-10 00:30:45 +0200

    refactored theme to use enliven-bootswatch

--- gulpfile.js
+++ gulpfile.js
@@ -185,7 +185,7 @@ gulp.task('serve', ['views', 'styles', 'scripts', 'fonts'], () => {
   gulp.watch('app/styles/**/*.scss', ['styles']);
   gulp.watch('app/scripts/**/*.js', ['scripts']);
   gulp.watch('app/fonts/**/*', ['fonts']);
-  gulp.watch('bower.json', ['wiredep', 'fonts']);
+  //gulp.watch('bower.json', ['wiredep', 'fonts']);
 });
 
 gulp.task('serve:dist', () => {
