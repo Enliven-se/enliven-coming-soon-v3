99691cf547ce47a74f32a90410a6c5581f9d5106
klokie <github@klokie.com>
2016-09-01 18:55:13 +0200

    EN-130: workaround for apparent bug in gulp-cache that prevented copying of a PNG

--- gulpfile.js
+++ gulpfile.js
@@ -113,15 +113,19 @@ gulp.task('html', ['views', 'styles', 'scripts'], () => {
 
 gulp.task('images', () => {
   return gulp.src('app/images/**/*')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true,
-      // don't remove IDs from SVGs, they are often used
-      // as hooks for embedding and styling
-      svgoPlugins: [{
-        cleanupIDs: false
-      }]
-    })))
+    .pipe(
+      //  $.cache(
+      $.imagemin({
+        progressive: true,
+        interlaced: true,
+        // don't remove IDs from SVGs, they are often used
+        // as hooks for embedding and styling
+        svgoPlugins: [{
+          cleanupIDs: false
+        }]
+      })
+  //  )
+  )
     .pipe(gulp.dest(destination + '/images'));
 });
 
